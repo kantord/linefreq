750ebc48476b56d743967b75a331a4fa86ae093d
Daniel Kantor <git@daniel-kantor.com>
2023-11-01 08:11:38 +0100

    feat: allow changing minimum count

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,15 @@ static ABOUT: &str =
 
 #[derive(Parser, Debug)]
 #[command(author, version, about = ABOUT, long_about = None)]
-struct Args {}
+struct Args {
+    /// Minimum number of times a line must occur to be printed
+    #[arg(short = 'm', long, default_value_t = 1)]
+    minimum_count: u128,
+}
 
 fn main() {
     let stdin = io::stdin();
-    Args::parse();
+    let args = Args::parse();
     let reader = stdin.lock();
     let mut counts: HashMap<String, u128> = HashMap::new();
 
@@ -21,6 +25,10 @@ fn main() {
     }
 
     for (key, value) in &counts {
-        println!("{},{}", key, value);
+        if *value < args.minimum_count {
+            continue;
+        }
+
+        println!("{}\t{}", key, value);
     }
 }
